886953fb1023a25f52c7d60f8c864c8336c1cde1
kimxwan0319 <kimxwan0319@naver.com>
2021-06-15 17:24:09 +0900

    ⚙️ :: 프로젝트 생성

---
+++ XQUAREDemo/XQUAREDemo/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  XQUAREDemo
+//
+//  Created by 김수완 on 2021/06/15.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
---
+++ XQUAREDemo/XQUAREDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  XQUAREDemo
+//
+//  Created by 김수완 on 2021/06/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
